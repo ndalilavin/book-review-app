a1ec99732176312b619f3dc2a09203f8c24e663f
ndalilavin <ndalila.steve@gmail.com>
2022-09-13 10:30:42 +0300

    creates card with a button adds shadow on the card adds background lightblue

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,10 +1,25 @@
-import CardCSS from './Card.module.css';
-import React from 'react';
+import CardCSS from "./Card.module.css";
+import React from "react";
+import image from "../../logo.svg";
 
-function Card(){
-    return (
-        <>
-        </>
-    )
+function Card() {
+  return (
+    <>
+      <div className={CardCSS.fullCard}>
+        <div className="columns is-multiline">
+          <div className="column is-4">
+            <div className={CardCSS.card}>
+              <div className={CardCSS.image}>               
+                <img src={image} alt="ProductName" />
+              </div>
+            <div className={CardCSS.buttons}>
+                <button>Rate</button>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-export default Card
+export default Card;
