9975348221746c011eacb87ef6d39709bf82265e
ndalilavin <ndalila.steve@gmail.com>
2022-09-13 17:50:18 +0300

    adds link to id

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,13 +1,15 @@
 import CardCSS from "./Card.module.css";
 import React from "react";
 import image from "../../logo.svg";
+import { Link } from "react-router-dom";
 
 function Card() {
   return (
     <>
+    <Link className={CardCSS.link} to={`/add-review/1`}>
       <div className={CardCSS.fullCard}>
         <div className="columns is-multiline">
-          <div className="column is-4">
+          <div className="column is-2">
             <div className={CardCSS.card}>
               <div className={CardCSS.image}>               
                 <img src={image} alt="ProductName" />
@@ -19,6 +21,7 @@ function Card() {
           </div>
         </div>
       </div>
+      </Link>
     </>
   );
 }
