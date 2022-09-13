92137d51107d052263b713368a20ea68be613bbc
ndalilavin <ndalila.steve@gmail.com>
2022-09-13 17:58:56 +0300

    create user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React,{ useState} from "react";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/home/Hero";
+import AddReview from "./components/addReview/AddReview";
+import { Route, Routes } from "react-router-dom";
+import User from "./components/input/User";
 
 function App() {
+  const [user, setUser] = useState("");
+
   return (
+   
     <div className="App">
-      <Navbar/>
-      <Hero/>
+      <User user={user} setUser={setUser}/>
+      {/* <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Hero />} />
+        <Route exact path="/add-review/:id" element={<AddReview />} />
+      </Routes> */}
     </div>
   );
 }
